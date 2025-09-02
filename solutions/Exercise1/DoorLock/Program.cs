fa507c5c331aea856cc71a1d166b651df76c0d3d
WilliamBZA <william.brander@gmail.com>
2025-09-02 13:57:37 +0200

    Add exercise 1

---
+++ solutions/Exercise1/DoorLock/Program.cs
@@ -0,0 +1,54 @@
+namespace DoorLock;
+
+using nanoFramework.Networking;
+using nanoFramework.WebServer;
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading;
+
+public class Program
+{
+    public static void Main()
+    {
+        WifiNetworkHelper.ConnectDhcp("dropitlikeaSquat", "DaisyToddAndButt", requiresDateTime: true);
+
+        var ipAddress = IPGlobalProperties.GetIPAddress().ToString();
+        Console.WriteLine($"IP Address: {ipAddress}");
+
+        using (WebServer server = new WebServer(80, HttpProtocol.Http, new[] { typeof(Controller) } ))
+        {
+            // Start the server.
+            server.Start();
+            
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
+
+public class Controller
+{
+    public static Random random = new Random();
+
+    [Route("api/unlockdoor")]
+    [Method("GET")]
+    public void UnlockDoor(WebServerEventArgs e)
+    {
+        var randomNumber = random.Next(100);
+        if (randomNumber <= 5)
+        {
+            WebServer.OutputHttpCode(e.Context.Response, HttpStatusCode.InternalServerError);
+            return;
+        }
+        else if (randomNumber <= 7)
+        {
+            Thread.Sleep(60000);
+        }
+
+        var response = "{\"IsUnlocked\": true}";
+        e.Context.Response.ContentType = "text/json";
+        WebServer.OutPutStream(e.Context.Response, response);
+        WebServer.OutputHttpCode(e.Context.Response, HttpStatusCode.OK);
+    }
+}
---
+++ solutions/Exercise1/HttpRequester/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Exercise1;
+
+using System;
+using System.Net.Http;
+using System.Text.Json;
+
+class Program
+{
+    private static readonly HttpClient httpClient = new HttpClient();
+
+    static async Task Main(string[] args)
+    {
+        int concurrency = 5;
+
+        // do it 1000 times
+        for (var x = 0; x < 1000 / concurrency; x++)
+        {
+            var tasks = new List<Task>();
+
+            for (var i = 0; i < concurrency; i++)
+            {
+                tasks.Add(MakeRequest(x * concurrency + i));
+            }
+
+            await Task.WhenAll(tasks);
+        }
+    }
+
+    static async Task MakeRequest(int attemptNumber)
+    {
+        try
+        {
+            var response = await httpClient.GetAsync("http://192.168.88.135/api/openchest");
+            response.EnsureSuccessStatusCode();
+
+            string jsonResponse = await response.Content.ReadAsStringAsync();
+
+            var openChestResponse = JsonSerializer.Deserialize<OpenChestResponse>(jsonResponse);
+            Console.WriteLine($"Attempt {attemptNumber} {DateTime.UtcNow.ToShortTimeString()} - ChestIsOpening = {openChestResponse!.IsOpening}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Failure on attempt {attemptNumber}");
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
+
+class OpenChestResponse
+{
+    public bool IsOpening { get; set; }
+}
+
+class DoorUnlockResponse
+{
+    public bool IsUnlocked { get; set; }
+}
