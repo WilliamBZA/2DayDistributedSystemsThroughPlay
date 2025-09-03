3438c743bd9cf399c622d77510c5ecec62295b99
WilliamBZA <william.brander@gmail.com>
2025-09-03 19:46:04 +0200

    Add exercise 11

---
+++ solutions/Exercise11/PuzzleController/Program.cs
@@ -0,0 +1,102 @@
+﻿using Azure.Messaging.ServiceBus;
+using MessagingHelper;
+using Microsoft.Data.SqlClient;
+using Puzzle.Messages;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace PuzzleController
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var connectionString = "connectionstring";
+
+            var helper = new ServiceBusHelper(connectionString);
+
+            await helper.EnsureQueueExists("williampuzzlecontroller");
+
+            var client = new ServiceBusClient(connectionString);
+            var processor = client.CreateProcessor("williampuzzlecontroller", new ServiceBusProcessorOptions
+            {
+                PrefetchCount = 10,
+                MaxConcurrentCalls = 10,
+            });
+
+            processor.ProcessErrorAsync += Processor_ProcessErrorAsync;
+
+            var stopWatch = new Stopwatch();
+            var numberOfMessagesProcessed = 0;
+
+            processor.ProcessMessageAsync += args =>
+            {
+                var currentCount = Interlocked.Increment(ref numberOfMessagesProcessed);
+                Console.WriteLine(currentCount);
+
+                if (currentCount >= 100)
+                {
+                    stopWatch.Stop();
+                    Console.WriteLine($"It took {stopWatch.ElapsedMilliseconds}ms to process 100 messages...");
+                }
+
+                return Task.CompletedTask;
+            };
+
+            await processor.StartProcessingAsync();
+            stopWatch.Start();
+
+            while (true)
+            {
+                Console.WriteLine("Press 'R' to send a ResetPattern message");
+                Console.WriteLine("Press a number to send a CaptureInput message with that number");
+                Console.WriteLine("Press any other key to exit");
+
+                await using var sender = client.CreateSender("williampuzzle");
+
+                var key = Console.ReadKey();
+                Console.WriteLine();
+
+                if (key.Key == ConsoleKey.R)
+                {
+                    var resetMessage = new ServiceBusMessage(JsonSerializer.Serialize(new ResetPattern()));
+                    resetMessage.ApplicationProperties["MessageType"] = "ResetPattern";
+
+                    await sender.SendMessageAsync(resetMessage);
+                    Console.WriteLine("ResetPuzzle message sent\n");
+                }
+                else if (char.IsDigit(key.KeyChar))
+                {
+                    var buttonNumber = int.Parse(key.KeyChar.ToString());
+                    var captureInputMessage = new ServiceBusMessage(JsonSerializer.Serialize(new CaptureInput { ButtonNumberPushed = buttonNumber }));
+                    captureInputMessage.ApplicationProperties["MessageType"] = "CaptureInput";
+
+                    await sender.SendMessageAsync(captureInputMessage);
+
+                    Console.WriteLine($"CaptureInput message with button number {buttonNumber} sent\n");
+                }
+            }
+        }
+
+        private static Task Processor_ProcessErrorAsync(ProcessErrorEventArgs arg)
+        {
+            Console.WriteLine("Error");
+            return Task.CompletedTask;
+        }
+
+        private static readonly HttpClient httpClient = new HttpClient();
+        static async Task MakeRequest(string url)
+        {
+            try
+            {
+                var response = await httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ solutions/Exercise11/SimulatedPuzzle/Program.cs
@@ -0,0 +1,129 @@
+﻿using Azure.Messaging.ServiceBus;
+using MessagingHelper;
+using Puzzle.Messages;
+using System.Runtime.ExceptionServices;
+using System.Text.Json;
+
+namespace SimulatedPuzzle
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var connectionString = "connectionstring";
+            await EnsureQueuesAndTopicsExist(connectionString);
+
+            var client = new ServiceBusClient(connectionString);
+            var processor = client.CreateProcessor("williampuzzle", new ServiceBusProcessorOptions
+            {
+            });
+
+            processor.ProcessErrorAsync += Processor_ProcessErrorAsync;
+            processor.ProcessMessageAsync += args =>
+            {
+                var messageType = args.Message.ApplicationProperties["MessageType"].ToString();
+                var body = args.Message.Body;
+
+                switch (messageType)
+                {
+                    case "ShowSequence":
+                        var showSequence = JsonSerializer.Deserialize<ShowSequence>(body);
+                        Console.WriteLine($"Show Puzzle Sequence");
+                        break;
+
+                    case "ShowSolved":
+                        var showSolved = JsonSerializer.Deserialize<ShowSolved>(body);
+                        Console.WriteLine($"Show solved");
+                        break;
+
+                    case "ShowFailed":
+                        var showFailed = JsonSerializer.Deserialize<ShowFailed>(body);
+                        Console.WriteLine($"Show failed");
+                        break;
+
+                    case "ResetPattern":
+                        var resetPattern = JsonSerializer.Deserialize<ResetPattern>(body);
+                        Console.WriteLine($"Reset Pattern");
+                        break;
+
+                    case "CaptureInput":
+                        var captureInput = JsonSerializer.Deserialize<CaptureInput>(body);
+                        Console.WriteLine($"Input captured - button number {captureInput.ButtonNumberPushed} pushed");
+                        break;
+
+                    default:
+                        Console.WriteLine("Unknown message type.");
+                        break;
+                }
+
+                return Task.CompletedTask;
+            };
+
+            await processor.StartProcessingAsync();
+
+            while (true)
+            {
+                Console.WriteLine("Press 'S' to send 100 messages");
+                Console.WriteLine("Press any other key to exit");
+
+                // sending here instead of in the helper because we're using batching
+                await using var sender = client.CreateSender("puzzleevents");
+
+                var key = Console.ReadKey();
+                Console.WriteLine();
+                switch (key.Key)
+                {
+                    case ConsoleKey.S:
+                        Console.WriteLine("Sending...");
+
+                        var batch = await sender.CreateMessageBatchAsync();
+                        for (var i = 0; i < 100; i++)
+                        {
+                            var message = new ServiceBusMessage($"Message {i}");
+                            if (!batch.TryAddMessage(message))
+                            {
+                                await sender.SendMessagesAsync(batch);
+                                batch = await sender.CreateMessageBatchAsync();
+                                batch.TryAddMessage(message);
+                            }
+                        }
+                        await sender.SendMessagesAsync(batch);
+
+                        Console.WriteLine("Message sent\n");
+                        break;
+
+                    default:
+                        return;
+                }
+            }
+        }
+
+        private static Task Processor_ProcessErrorAsync(ProcessErrorEventArgs arg)
+        {
+            Console.WriteLine("Error");
+            return Task.CompletedTask;
+        }
+
+        private static async Task EnsureQueuesAndTopicsExist(string connectionString)
+        {
+            var helper = new ServiceBusHelper(connectionString);
+
+            // Make sure the queues exist
+            await helper.EnsureQueueExists("williampuzzlecontroller");
+            await helper.EnsureQueueExists("williamdashboard");
+            await helper.EnsureQueueExists("williampuzzle");
+
+            // Make sure the topic exists
+            await helper.EnsureTopicExists("puzzleevents");
+
+            // Make sure the subscriptions exist
+            await helper.EnsureSubscriptionExists(topicName: "puzzleevents",
+               subscriptionName: "williampuzzlecontroller-subscription",
+               queueToForwardTo: "williampuzzlecontroller");
+
+            await helper.EnsureSubscriptionExists(topicName: "puzzleevents",
+               subscriptionName: "williamdashboard-subscription",
+               queueToForwardTo: "williamdashboard");
+        }
+    }
+}
